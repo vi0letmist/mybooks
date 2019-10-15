85972f512d15856c2bd58fbcbfc2e197f51184d6
Friday Andita <fridayars@gmail.com>
2019-10-15 13:47:15 +0700

    migration

---
+++ database/migrations/2019_10_15_063255_create_bukus_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBukusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul');
+            $table->string('penulis');
+            $table->string('deskripsi');
+            $table->date('tgl_tambah_buku');
+            $table->string('sampul');
+            $table->string('penerbit');
+            $table->integer('tahun_terbit');
+            $table->integer('rak_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bukus');
+    }
+}
--- database/migrations/2019_10_15_063523_create_pinjam_bukus_table.php
+++ database/migrations/2019_10_15_063523_create_pinjam_bukus_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePinjamBukusTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        // Schema::dropIfExists('users');
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pinjam_bukus', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->tinyInteger('status');
+            $table->date('tgl_pinjam');
+            $table->date('tgl_kembali');
+            $table->integer('buku_id');
+            $table->integer('peminjam_id');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pinjam_bukus');
     }
 }
