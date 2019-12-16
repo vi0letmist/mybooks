2433ad99980ec621058863fca40ee1f1d045633b
unknown <jaffar.jatmiko.jati@mail.ugm.ac.id>
2019-12-16 13:45:22 +0700

    some view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,19 +6,19 @@
 ])
 
 @section('content')
-  <div class="panel-header panel-header-lg">
-    <canvas id="bigDashboardChart"></canvas>
+  <div class="panel-header panel-header-lg" style="margin-bottom:-20em;">
+    <!-- <canvas id="bigDashboardChart"></canvas> -->
   </div>
   <div class="content">
     <div class="row">
-      <div class="col-lg-4">
-        <div class="card card-chart">
+      <div class="col-lg-3">
+        <div class="card card-chart" style="background-color:#eebc5a;">
           <div class="card-header">
-            <h5 class="card-category">Book</h5>
-            <h4 class="card-title">Books</h4>
+            <h5 class="card-category" style="color:#fff;">Book</h5>
+            <h4 class="card-title" style="color:#fff;">Books</h4>
             <div class="dropdown">
-              <button type="button" class="btn btn-round btn-outline-default dropdown-toggle btn-simple btn-icon no-caret" data-toggle="dropdown">
-                <i class="now-ui-icons loader_gear"></i>
+              <button type="button" class="btn btn-round btn-outline-default dropdown-toggle btn-simple btn-icon no-caret" data-toggle="dropdown" style="color:#fff;">
+                <i class="now-ui-icons loader_gear" style="color:#fff;"></i>
               </button>
               <div class="dropdown-menu dropdown-menu-right">
                 <a class="dropdown-item" href="#">Action</a>
@@ -29,26 +29,26 @@
             </div>
           </div>
           <div class="card-body">
-            <h1>{{$buku}}</h1>
-            <div class="chart-area">
+            <h1 style="color:#fff;">{{$buku}}</h1>
+            <!-- <div class="chart-area">
               <canvas id="lineChartExample"></canvas>
-            </div>
+            </div> -->
           </div>
           <div class="card-footer">
-            <div class="stats">
+            <div class="stats" style="color:#fff;">
               <i class="now-ui-icons arrows-1_refresh-69"></i> Just Updated
             </div>
           </div>
         </div>
       </div>
-      <div class="col-lg-4 col-md-6">
-        <div class="card card-chart">
+      <div class="col-lg-3 col-md-6">
+        <div class="card card-chart" style="background-color:#83e53a;">
           <div class="card-header">
-            <h5 class="card-category">Borrower</h5>
-            <h4 class="card-title">Borrowers</h4>
+            <h5 class="card-category" style="color:#fff;">Borrower</h5>
+            <h4 class="card-title" style="color:#fff;">Borrowers</h4>
             <div class="dropdown">
-              <button type="button" class="btn btn-round btn-outline-default dropdown-toggle btn-simple btn-icon no-caret" data-toggle="dropdown">
-                <i class="now-ui-icons loader_gear"></i>
+              <button type="button" class="btn btn-round btn-outline-default dropdown-toggle btn-simple btn-icon no-caret" data-toggle="dropdown" style="color:#fff;">
+                <i class="now-ui-icons loader_gear" style="color:#fff;"></i>
               </button>
               <div class="dropdown-menu dropdown-menu-right">
                 <a class="dropdown-item" href="#">Action</a>
@@ -59,20 +59,20 @@
             </div>
           </div>
           <div class="card-body">
-            <h1>12</h1>
-            <div class="chart-area">
+            <h1 style="color:#fff;">12</h1>
+            <!-- <div class="chart-area">
               <canvas id="lineChartExampleWithNumbersAndGrid"></canvas>
-            </div>
+            </div> -->
 
           </div>
           <div class="card-footer">
-            <div class="stats">
-              <i class="now-ui-icons arrows-1_refresh-69"></i> Just Updated
+            <div class="stats" style="color:#fff;">
+              <i class="now-ui-icons arrows-1_refresh-69" style="color:#fff;"></i> Just Updated
             </div>
           </div>
         </div>
       </div>
-      <div class="col-lg-4">
+      <div class="col-lg-12">
         <div class="card">
           <div class="card-header">
             <h5 class="card-category">All Persons List</h5>
@@ -126,8 +126,7 @@
       </div>
     </div>
     <div class="row">
-
-    </div>
+    
   </div>
 @endsection
 
